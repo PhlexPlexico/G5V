79acdfc7a516876c5772f74f5a8b829e41e5a587
PhlexPlexico <flexing@phlexplexi.co>
2020-11-14 12:45:58 -0600

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
   devServer: {
-    public: "http://204.83.44.105:8080",
+    public: "http://localhost:8080",
     proxy: {
       "^/api": {
-        target: "http://204.83.44.105:3301",
+        target: "http://localhost:3301",
         changeOrigin: true,
         pathRewrite: {
           "^/api": ""
