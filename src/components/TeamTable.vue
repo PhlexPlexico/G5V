bfe46e927e628f0e2a111cd251626b00753e7ddc
PhlexPlexico <flexing@phlexplexi.co>
2021-01-15 10:30:49 -0600

    Lowercase to show team flags. Leave everything else as is.

--- src/components/TeamTable.vue
+++ src/components/TeamTable.vue
@@ -369,7 +369,7 @@ export default {
         this.teamInfo = {
           id: res.id,
           name: res.name,
-          flag: res.flag == null ? null : res.flag,
+          flag: res.flag == null ? null : res.flag.toUpperCase(),
           logo: res.logo,
           tag: res.tag,
           public: res.public_team,
