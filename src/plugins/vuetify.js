d1b5d570ca1bc0b28db1016abf85464ff9cbf85a
Ashutosh Kumar <kancha@lotgaming.xyz>
2023-07-24 19:52:25 +0530

    erewww

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -6,15 +6,21 @@ Vue.use(Vuetify);
 
 export default new Vuetify({
   theme: {
+    defaultTheme: "dark",
     themes: {
-      dark: {
-        primary: colors.green.darken1,
+      light: {
+        primary: colors.blue,
         secondary: colors.blueGrey.darken1,
         accent: colors.shades.black,
         error: colors.red.accent3,
+        background: colors.lightBlue.lighten4,
+        footer: colors.lightBlue
+      },
+      dark: {
+        primary: colors.green.darken1,
         background: colors.lime.darken4,
         footer: colors.green.darken1
-      },
+      }
     },
   }
 });
