82093b751ed3fc39c98e7102db180c78866373bc
Ashutosh Kumar <kancha@lotgaming.xyz>
2023-07-24 12:15:28 +0530

    color test

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -8,12 +8,12 @@ export default new Vuetify({
   theme: {
     themes: {
       light: {
-        primary: colors.blue,
-        secondary: colors.blueGrey.darken1,
-        accent: colors.shades.black,
-        error: colors.red.accent3,
-        background: colors.lightBlue.lighten4,
-        footer: colors.lightBlue
+        primary: colors.red,
+        secondary: colors.red.lighten2,
+        accent: colors.red.accent4,
+        error: colors.red.lighten5,
+        background: colors.pink.lighten1,
+        footer: colors.pink.accent4
       },
       dark: {
         primary: colors.teal,
